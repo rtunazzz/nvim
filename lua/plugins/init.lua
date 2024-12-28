938111f4fba91448e8238ad1dac49b7ffaba6d6b
rtuna <artur.hnat@gmail.com>
2024-12-28 14:24:42 +0100

    chore: update deps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -47,15 +47,21 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
-    opts = {},
+    opts = {
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.5-sonnet",
+        -- max_tokens = 4096,
+      },
+    },
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below is optional, make sure to setup it properly if you have lazy=true
+      "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      "zbirenbaum/copilot.lua",    -- for providers='copilot'
       {
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {
