65541ac21cb724cd3026290140577a0b0530a318
rtuna <artur.hnat@gmail.com>
2025-03-09 11:56:13 +0100

    feat: add run_go_tests tool

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -55,6 +55,41 @@ return {
         temperature = 1,
         max_tokens = 20000,
       },
+      custom_tools = {
+        {
+          name = "run_go_tests",                            -- Unique name for the tool
+          description = "Run Go unit tests and return results", -- Description shown to AI
+          command = "go test -v ./...",                     -- Shell command to execute
+          param = {                                         -- Input parameters (optional)
+            type = "table",
+            fields = {
+              {
+                name = "target",
+                description = "Package or directory to test (e.g. './pkg/...' or './internal/pkg')",
+                type = "string",
+                optional = true,
+              },
+            },
+          },
+          returns = { -- Expected return values
+            {
+              name = "result",
+              description = "Result of the fetch",
+              type = "string",
+            },
+            {
+              name = "error",
+              description = "Error message if the fetch was not successful",
+              type = "string",
+              optional = true,
+            },
+          },
+          func = function(params, on_log, on_complete) -- Custom function to execute
+            local target = params.target or "./..."
+            return vim.fn.system(string.format("go test -v %s", target))
+          end,
+        },
+      }
     },
     dependencies = {
       "stevearc/dressing.nvim",
