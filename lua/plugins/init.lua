82908851261cd68446d6579749cf33c8c119627c
rtuna <artur.hnat@gmail.com>
2025-05-31 12:03:07 +0200

    feat: add surround plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -44,6 +44,11 @@ return {
   },
 
   {
+    "tpope/vim-surround",
+    event = "VeryLazy"
+  },
+
+  {
     "yetone/avante.nvim",
     event = "VeryLazy",
     build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
