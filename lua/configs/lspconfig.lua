ee0b367cd6fd9066a130d7c2e78d4f097326adae
rtuna <artur.hnat@gmail.com>
2025-09-20 15:05:42 +0200

    fix: nvim 0.11

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,26 +1,16 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 local servers = { "html", "cssls", "ts_ls", "clangd", "bashls", "pyright", "dockerls", "solidity" }
-local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  vim.lsp.config(lsp, {})
+  vim.lsp.enable(lsp)
 end
 
 -- custom LSP setups
-
-lspconfig.gopls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
+vim.lsp.config("gopls", {
   settings = {
     gopls = {
       gofumpt = true, -- A stricter gofmt
@@ -32,4 +22,5 @@ lspconfig.gopls.setup {
       buildFlags = { "-tags=integration" }
     },
   },
-}
+})
+vim.lsp.enable("gopls")
